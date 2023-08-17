dfa852baaea3b89773418e023a21b92d3d0aa9c6
TMaddox <20703314+TMaddox@users.noreply.github.com>
2023-08-17 07:56:01 +0200

    added comments

--- main.m
+++ main.m
@@ -1,87 +1,87 @@
-clc; clearvars; close all;
+clc; clearvars; close all; % clear console; clear varibles; figures schliessen
 fprintf('start\n')
 
 % solver settings
-max_iter = 10000;
-CFL = 0.1;
-N_i = 15; % number of nodes in R direction
-N_j = N_i * 4; % number of nodes in Phi direction
-alpha = 0.2; % relaxation for velocity
-beta = 0.2; % relaxation for pressure
-GS_max_iter = 100;
+max_iter = 10000;       % max. # Iterationen
+CFL = 0.1;              % Courant-Friedrichs-Lewy Kriterium
+N_i = 15;               % # of nodes in R-Richtung
+N_j = N_i * 4;          % # of nodes in Phi-Richtung
+alpha = 0.2;            % Relaxation für velocity
+beta = 0.2;             % Relaxation für pressure
+GS_max_iter = 100;      % max. # Iterationen für Gauss-Seidel solver
 
 % settings
-T_h_absolut = 400;
-T_c_absolut = 300;
-R = 1;
-R_a = R;
-R_i = R / 2;
-Phi_start = pi;
-Phi_end = 2*pi;
-Re = 60;
-Pr = 4;
+T_h_absolut = 400;      % max. Temperatur (Beispielhaft, normierung erfolgt später)
+T_c_absolut = 300;      % min. Temperatur (Beispielhaft, normierung erfolgt später)
+R = 1;                  % Radius (Beispielhaft)
+R_a = R;                % Radius aussen (lt. Angabe)
+R_i = R / 2;            % Radius innen (lt. Angabe)
+Phi_start = pi;         % Startwinkel (lt. Angabe)
+Phi_end = 2*pi;         % Endwinkel (lt. Angabe)
+Re = 60;                % Reynolds Zahl (lt. Angabe)
+Pr = 4;                 % Prandtl Zahl (lt. Angabe)
 
 % add ghost cells
 i_max = N_i + 2;
 j_max = N_j + 2;
-i_fixed = 3; % one p_corr needs to be set to a constant fixed value because only neumann boundaries are present
+i_fixed = 3; % one p_corr needs to be set to a constant fixed value because only Neumann boundaries are present
 j_fixed = 3;
 
 % wall temperatures
-T_h = (T_h_absolut - 300) / (400 - 300);
-T_c = (T_c_absolut - 300) / (400 - 300);
+T_h = (T_h_absolut - T_c_absolut) / (T_h_absolut - T_c_absolut);    % normierung Temperatur
+T_c = (T_c_absolut - T_c_absolut) / (T_h_absolut - T_c_absolut);    % normierung Temperatur
 % wall velocity
-v_wall_max = 1;
+v_wall_max = 1;         % Geschwindigkeit bewegte Wand (Beispielhaft)
 
-% grid
-dr = (R_a - R_i) / N_i;
-rp = linspace(R_i - dr/2, R_a + dr/2, i_max);
-ru = linspace(R_i, R_a + dr, i_max);
-dphi = (Phi_end - Phi_start) / N_j;
-phip = linspace(Phi_start - dphi/2, Phi_end + dphi/2, j_max);
-phiv = linspace(Phi_start, Phi_end + dphi, j_max);
-[R_coord, Phi_coord] = meshgrid(linspace(R_i-dr/2, R_a+dr/2, i_max), linspace(Phi_start-dphi/2, Phi_end+dphi/2, j_max));
-R_coord = transpose(R_coord);
-Phi_coord = transpose(Phi_coord);
+% Grid Parameter
+dr = (R_a - R_i) / N_i;                             % Grid size in R-Richtung
+rp = linspace(R_i - dr/2, R_a + dr/2, i_max);       % linspace von allen rp's (Radius aller Points)
+ru = linspace(R_i, R_a + dr, i_max);                % linspace von allen ru's (Radius aller Zellwände)
+dphi = (Phi_end - Phi_start) / N_j;                 % Grid size in phi-Richtung
+phip = linspace(Phi_start - dphi/2, Phi_end + dphi/2, j_max);   % linspace von allen phip's (Winkel aller Points)
+phiv = linspace(Phi_start, Phi_end + dphi, j_max);              % linspace von allen phiv's (Winkel aller Zellwände)
+[R_coord, Phi_coord] = meshgrid(linspace(R_i-dr/2, R_a+dr/2, i_max), linspace(Phi_start-dphi/2, Phi_end+dphi/2, j_max)); % Meshgrid aller Nodes (incl. ghost cells)
+R_coord = transpose(R_coord);                       % Korrektur
+Phi_coord = transpose(Phi_coord);                   % Korrektur
 
 % small functions
-avg = @(a, b) (a+b)/2; % average between two values
+avg = @(a, b) (a+b)/2;          % average between two values
 
-% other variables
-fconti = NaN(max_iter, 1);
-fp_corr = NaN(max_iter, 1);
+% other variables for plotting
+fconti = NaN(max_iter, 1);      % residual_der_kontinuitätsgleichung(iter)
+fp_corr = NaN(max_iter, 1);     % p_corr(iter)
 
 % time difference per cell
-dt = NaN(i_max, j_max);
+dt = NaN(i_max, j_max);         % Zeitschritt per cell
 
 % n + 1 timestep
-u_np1 = NaN(i_max, j_max);
-v_np1 = NaN(i_max, j_max);
-p_np1 = NaN(i_max, j_max);
-T_np1 = NaN(i_max, j_max);
+u_np1 = NaN(i_max, j_max);      % Geschwindigkeit u (in r-Richtung)
+v_np1 = NaN(i_max, j_max);      % Geschwindigkeit v (in phi-Richtung)
+p_np1 = NaN(i_max, j_max);      % Druck
+T_np1 = NaN(i_max, j_max);      % Temperatur
 
-% current timestep
-u_n = ones(i_max, j_max) * 0;
-v_n = ones(i_max, j_max) * 0;
-p_n = ones(i_max, j_max) * 0;
-T_n = ones(i_max, j_max) * 0.5;
-Fi_n = zeros(i_max, j_max);
-Fj_n = zeros(i_max, j_max);
-Fe_n = zeros(i_max, j_max);
+% current (n) timestep
+u_n = ones(i_max, j_max) * 0;   % Geschwindigkeit u (in r-Richtung)
+v_n = ones(i_max, j_max) * 0;   % Geschwindigkeit v (in phi-Richtung)
+p_n = ones(i_max, j_max) * 0;   % Druck
+T_n = ones(i_max, j_max) * 0.5; % Temperatur
+Fi_n = zeros(i_max, j_max);     % RHS der Impulserhaltungsgleichung in R-Richtung zum Zeitpunkt n
+Fj_n = zeros(i_max, j_max);     % RHS der Impulserhaltungsgleichung in phi-Richtung zum Zeitpunkt n
+Fe_n = zeros(i_max, j_max);     % RHS der Energiegleichung zum Zeitpunkt n
 
 % n - 1 timestep
-u_nm1 = ones(i_max, j_max) * 0;
-v_nm1 = ones(i_max, j_max) * 0;
-p_nm1 = ones(i_max, j_max) * 0;
-T_nm1 = ones(i_max, j_max) * 0.5;
-Fi_nm1 = zeros(i_max, j_max);
-Fj_nm1 = zeros(i_max, j_max);
-Fe_nm1 = zeros(i_max, j_max);
+u_nm1 = ones(i_max, j_max) * 0; % Geschwindigkeit u (in r-Richtung)
+v_nm1 = ones(i_max, j_max) * 0; % Geschwindigkeit v (in phi-Richtung)
+p_nm1 = ones(i_max, j_max) * 0; % Druck
+T_nm1 = ones(i_max, j_max) * 0.5;   % Temperatur
+Fi_nm1 = zeros(i_max, j_max);   % RHS der Impulserhaltungsgleichung in R-Richtung zum Zeitpunkt n-1
+Fj_nm1 = zeros(i_max, j_max);   % RHS der Impulserhaltungsgleichung in phi-Richtung zum Zeitpunkt n-1
+Fe_nm1 = zeros(i_max, j_max);   % RHS der Energiegleichung zum Zeitpunkt n-1
 
 % projection step
-u_star = NaN(i_max, j_max);
-v_star = NaN(i_max, j_max);
-p_corr = NaN(i_max, j_max);
+u_star = NaN(i_max, j_max);     % Geschwindigkeit u* (in r-Richtung) im Zeitpunkt *
+v_star = NaN(i_max, j_max);     % Geschwindigkeit v* (in phi-Richtung) im Zeitpunkt *
+p_corr = NaN(i_max, j_max);     % Druck für Druckkorrektur (für * --> n+1)
 
 % apply initial boundaries
 u_n = apply_u_boundary(u_n, i_max, j_max);
@@ -99,11 +99,11 @@ for i = 2:i_max-1
     for j = 2:j_max-1
         if i ~= i_fixed || j ~= j_fixed
             idx = j + (i-1) * j_max;
-            A(idx, idx) = - ru(i)*dphi/dr - ru(i-1)*dphi/dr - 2 * dr/(rp(i)*dphi);
-            A(idx, idx+1) = ru(i)*dphi/dr;
-            A(idx, idx-1) = ru(i-1)*dphi/dr;
-            A(idx, idx+j_max) = dr/(rp(i)*dphi);
-            A(idx, idx-j_max) = dr/(rp(i)*dphi);
+            A(idx, idx) = - ru(i)*dphi/dr - ru(i-1)*dphi/dr - 2*dr/(rp(i)*dphi);  % i,j
+            A(idx, idx+1) = ru(i)*dphi/dr;          % i+1,j
+            A(idx, idx-1) = ru(i-1)*dphi/dr;        % i-1,j
+            A(idx, idx+j_max) = dr/(rp(i)*dphi);    % i,j+1
+            A(idx, idx-j_max) = dr/(rp(i)*dphi);    % i,j-1
         end
     end
 end
@@ -292,7 +292,7 @@ for itr = 1:max_iter
     end
 
     % calculate v_wall as a function of iter for smooth start
-    v_wall = v_wall_max * (1 - exp(-itr/500));
+    v_wall = v_wall_max * (1 - exp(-itr/(max_iter/20)));
 
     % calculate dt per cell (local timestep)
     for i = 2:i_max-1
@@ -302,6 +302,7 @@ for itr = 1:max_iter
     end
 
     % %%%%%%%% PROJECTION %%%%%%%%
+    % RHS for r-impulse & projection u*
     for i = 2:i_max-2
         for j = 2:j_max-1
             % RHS for r-impulse
@@ -322,10 +323,11 @@ for itr = 1:max_iter
                 + u_n(i,j)) * dr * dphi;
             Fi_n(i,j) = - i_t1 - i_t2 + i_t3 - i_t4 + i_t5 + i_t6 - i_t7;
 
-            % projection
+            % projection u*
             u_star(i,j) = dt(i,j)/(dr*dphi*ru(i)) * (3/2 * Fi_n(i,j) - 1/2 * Fi_nm1(i,j)) + u_n(i,j);
         end
     end
+    % RHS fpr phi-impulse & projection v*
     for i = 2:i_max-1
         for j = 2:j_max-2
             % RHS for phi-impulse
@@ -345,17 +347,16 @@ for itr = 1:max_iter
                 - avg(u_n(i,j), u_n(i-1,j))) * dr;
             Fj_n(i,j) = - j_t1 - j_t2 - j_t3 - j_t4 + j_t5 + j_t6;
 
-            % projection
+            % projection v*
             v_star(i,j) = dt(i,j)/(dr*dphi*rp(i)) * (3/2 * Fj_n(i,j) - 1/2 * Fj_nm1(i,j)) + v_n(i,j);
         end
     end
 
-    % apply boundaries
+    % apply boundaries for projected velocities
     u_star = apply_u_boundary(u_star, i_max, j_max);
     v_star = apply_v_boundary(v_star, i_max, j_max, v_wall);
 
     % %%%%%%%% POISSON %%%%%%%%
-    p_corr_tmp = zeros(i_max*j_max, 1);
     b = zeros(i_max*j_max, 1);
     for i = 2:i_max-1
         for j = 2:j_max-1
@@ -368,8 +369,9 @@ for itr = 1:max_iter
     end
 
     % gauss seidl solver for solving the A*x=b Eq. system
+    p_corr_tmp = zeros(i_max*j_max, 1);
     p_corr_tmp = GS(A, b, p_corr_tmp, GS_max_iter);
-    %p_corr_tmp = A \ b;
+    % p_corr_tmp = A \ b;
 
     % reshape solution into correct dimensions
     p_corr = reshape(p_corr_tmp, [j_max, i_max])';
@@ -377,8 +379,24 @@ for itr = 1:max_iter
     p_corr(i_max, 1) = nan;
     p_corr(1, j_max) = nan;
     p_corr(i_max, j_max) = nan;
- 
+
     % %%%%%%%% CORRECTION %%%%%%%%
+    % correct u
+    for i = 2:i_max-2
+        for j = 2:j_max-1
+            u_np1(i,j) = u_star(i,j) - dt(i,j)/dr * (p_corr(i+1,j) - p_corr(i,j)) * alpha;
+        end
+    end
+    % correct v
+    for i = 2:i_max-1
+        for j = 2:j_max-2
+            v_np1(i,j) = v_star(i,j) - dt(i,j)/(dphi * rp(i)) * (p_corr(i,j+1) - p_corr(i,j)) * alpha;
+        end
+    end
+    % correct pressure
+    p_np1 = p_n + p_corr * beta;
+
+    % %%%%%%%% CALCULATE ENERGY EQUATION %%%%%%%%
     for i = 2:i_max-1
         for j = 2:j_max-1
             % RHS for energy
@@ -393,19 +411,6 @@ for itr = 1:max_iter
             T_np1(i,j) = dt(i,j)/(dr*dphi*ru(i)) * (3/2 * Fe_n(i,j) - 1/2 * Fe_nm1(i,j)) + T_n(i,j);
         end
     end
-    for i = 2:i_max-2
-        for j = 2:j_max-1
-            % correction
-            u_np1(i,j) = u_star(i,j) - dt(i,j)/dr * (p_corr(i+1,j) - p_corr(i,j)) * alpha;
-        end
-    end
-    for i = 2:i_max-1
-        for j = 2:j_max-2
-            % correction
-            v_np1(i,j) = v_star(i,j) - dt(i,j)/(dphi * rp(i)) * (p_corr(i,j+1) - p_corr(i,j)) * alpha;
-        end
-    end
-    p_np1 = p_n + p_corr * beta;
 
     % %%%%%%%% STORE VALUES %%%%%%%%
     % copy values from timestep n to n-1 for next iter
@@ -440,7 +445,7 @@ for itr = 1:max_iter
     fprintf('max(abs(conti)): %i\n', max(max(abs(conti))))
     %fprintf('max(abs(p_corr)): %i\n', max(max(abs(p_corr))))
 
-    % 
+    % track convergence metrics over iter
     fconti(itr) = max(max(abs(conti)));
     fp_corr(itr) = max(max(abs(p_corr)));
 end
